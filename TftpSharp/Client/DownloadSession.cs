be2d317d647240dc3d11a157ac8d93a0155983a1
bojan.malinic <bojan.malinic@outlook.com>
2024-01-01 23:42:48 +0100

    Extract common methods into Session class

--- TftpSharp/Client/DownloadSession.cs
+++ TftpSharp/Client/DownloadSession.cs
@@ -13,17 +13,15 @@ using TftpSharp.Packet;
 
 namespace TftpSharp.Client
 {
-    internal class DownloadSession
+    internal class DownloadSession : Session
     {
-        private readonly UdpClient _udpClient;
         private readonly string _host;
         private readonly string _filename;
         private readonly TransferMode _transferMode;
         private readonly Stream _stream;
 
-        public DownloadSession(UdpClient udpClient, string host, string filename, TransferMode transferMode, Stream stream)
+        public DownloadSession(UdpClient udpClient, string host, string filename, TransferMode transferMode, Stream stream) : base(udpClient)
         {
-            _udpClient = udpClient;
             _host = host;
             _filename = filename;
             _transferMode = transferMode;
@@ -119,52 +117,6 @@ namespace TftpSharp.Client
 
             await AckAndDally(lastRecvDataPacket.BlockNumber, remoteEndpoint, cancellationToken);
 
-        }
-
-        private async Task<(Packet.Packet receivedPacket, IPEndPoint remoteEndpoint)> SendAndReceiveWithRetry(
-            Packet.Packet packet, IPEndPoint endpoint, Func<CancellationToken, Task<(Packet.Packet packet, IPEndPoint endpoint)>> receiveOperation, 
-            int timeout, int transmitAttempts, CancellationToken cancellationToken = default)
-        {
-            bool retry;
-            IPEndPoint? recvEndpoint = null;
-            Packet.Packet? receivedPacket = null;
-            var attempts = transmitAttempts;
-
-
-            do
-            {
-                try
-                {
-                    await _udpClient.SendTftpPacketAsync(packet, endpoint, cancellationToken);
-                    (receivedPacket, recvEndpoint) = await WithTimeout(receiveOperation, timeout, cancellationToken);
-                    retry = false;
-                }
-                catch (ReceiveTimeoutException)
-                {
-                    retry = true;
-                }
-
-            } while (retry && --attempts > 0);
-
-            if(attempts == 0)
-                throw new TftpTimeoutException(transmitAttempts);
-
-            return (receivedPacket!, recvEndpoint!);
-        }
-
-        public static async Task<TResult> WithTimeout<TResult>(Func<CancellationToken, Task<TResult>> operation, int timeout, CancellationToken cancellationToken = default)
-        {
-            using var rcvTaskCancellationSource = new CancellationTokenSource();
-            await using var cancellationTokenRegistration = cancellationToken.Register(() => rcvTaskCancellationSource.Cancel());
-            var receiveTask = operation(rcvTaskCancellationSource.Token);
-            var timeoutTask = Task.Delay(timeout, cancellationToken);
-
-            var resultTask = await Task.WhenAny(receiveTask, timeoutTask);
-            if (resultTask == receiveTask)
-                return await receiveTask;
-
-            rcvTaskCancellationSource.Cancel();
-            throw new ReceiveTimeoutException();
         }
 
         private async Task AckAndDally(ushort ackBlock, IPEndPoint remoteEndpoint, CancellationToken cancellationToken = default)
---
+++ TftpSharp/Client/Session.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using TftpSharp.Exceptions;
+using TftpSharp.Extensions;
+
+namespace TftpSharp.Client
+{
+    internal abstract class Session
+    {
+
+        protected readonly UdpClient _udpClient;
+
+        protected Session(UdpClient udpClient)
+        {
+            _udpClient = udpClient;
+        }
+
+        protected async Task<(Packet.Packet receivedPacket, IPEndPoint remoteEndpoint)> SendAndReceiveWithRetry(
+            Packet.Packet packet, IPEndPoint endpoint, Func<CancellationToken, Task<(Packet.Packet packet, IPEndPoint endpoint)>> receiveOperation,
+            int timeout, int transmitAttempts, CancellationToken cancellationToken = default)
+        {
+            bool retry;
+            IPEndPoint? recvEndpoint = null;
+            Packet.Packet? receivedPacket = null;
+            var attempts = transmitAttempts;
+
+
+            do
+            {
+                try
+                {
+                    await _udpClient.SendTftpPacketAsync(packet, endpoint, cancellationToken);
+                    (receivedPacket, recvEndpoint) = await WithTimeout(receiveOperation, timeout, cancellationToken);
+                    retry = false;
+                }
+                catch (ReceiveTimeoutException)
+                {
+                    retry = true;
+                }
+
+            } while (retry && --attempts > 0);
+
+            if (attempts == 0)
+                throw new TftpTimeoutException(transmitAttempts);
+
+            return (receivedPacket!, recvEndpoint!);
+        }
+
+        protected static async Task<TResult> WithTimeout<TResult>(Func<CancellationToken, Task<TResult>> operation, int timeout, CancellationToken cancellationToken = default)
+        {
+            using var rcvTaskCancellationSource = new CancellationTokenSource();
+            await using var cancellationTokenRegistration = cancellationToken.Register(() => rcvTaskCancellationSource.Cancel());
+            var receiveTask = operation(rcvTaskCancellationSource.Token);
+            var timeoutTask = Task.Delay(timeout, cancellationToken);
+
+            var resultTask = await Task.WhenAny(receiveTask, timeoutTask);
+            if (resultTask == receiveTask)
+                return await receiveTask;
+
+            rcvTaskCancellationSource.Cancel();
+            throw new ReceiveTimeoutException();
+        }
+    }
+}
