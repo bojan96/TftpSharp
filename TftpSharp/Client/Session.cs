b96b2db569e6029e5fdd40d2e4e705bf7d4622aa
bojan.malinic <bojan.malinic@outlook.com>
2024-02-28 23:37:02 +0100

    Remove unnecessary code

--- TftpSharp/Client/Session.cs
+++ TftpSharp/Client/Session.cs
@@ -2,10 +2,8 @@
 using System.Net;
 using System.Threading.Tasks;
 using System.Threading;
-using System;
 using System.Linq;
 using TftpSharp.Exceptions;
-using TftpSharp.Extensions;
 
 namespace TftpSharp.Client;
 
@@ -17,52 +15,6 @@ internal abstract class Session
     protected Session(UdpClient udpClient)
     {
         _udpClient = udpClient;
-    }
-
-    protected async Task<(Packet.Packet receivedPacket, IPEndPoint remoteEndpoint)> SendAndReceiveWithRetry(
-        Packet.Packet packet, IPEndPoint endpoint, Func<CancellationToken, Task<(Packet.Packet packet, IPEndPoint endpoint)>> receiveOperation,
-        int timeout, int transmitAttempts, CancellationToken cancellationToken = default)
-    {
-        bool retry;
-        IPEndPoint? recvEndpoint = null;
-        Packet.Packet? receivedPacket = null;
-        var attempts = transmitAttempts;
-
-
-        do
-        {
-            try
-            {
-                await _udpClient.SendTftpPacketAsync(packet, endpoint, cancellationToken);
-                (receivedPacket, recvEndpoint) = await WithTimeout(receiveOperation, timeout, cancellationToken);
-                retry = false;
-            }
-            catch (ReceiveTimeoutException)
-            {
-                retry = true;
-            }
-
-        } while (retry && --attempts > 0);
-
-        if (attempts == 0)
-            throw new TftpTimeoutException(transmitAttempts);
-
-        return (receivedPacket!, recvEndpoint!);
-    }
-
-    protected static async Task<TResult> WithTimeout<TResult>(Func<CancellationToken, Task<TResult>> operation, int timeout, CancellationToken cancellationToken = default)
-    {
-        using var rcvTaskCancellationSource = new CancellationTokenSource();
-        await using var cancellationTokenRegistration = cancellationToken.Register(() => rcvTaskCancellationSource.Cancel());
-        var receiveTask = operation(rcvTaskCancellationSource.Token);
-        var timeoutTask = Task.Delay(timeout, cancellationToken);
-
-        var resultTask = await Task.WhenAny(receiveTask, timeoutTask);
-        if (resultTask == receiveTask)
-            return await receiveTask;
-
-        rcvTaskCancellationSource.Cancel();
-        throw new ReceiveTimeoutException();
     }
 
     protected static async Task<IPAddress> ResolveHostAsync(string host, CancellationToken cancellationToken = default)
