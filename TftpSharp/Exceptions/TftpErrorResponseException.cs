2291ac972001974e4a205a441abc0f85d00864e4
bojan.malinic <bojan.malinic@outlook.com>
2024-04-15 01:14:39 +0200

    Expose error properties

--- TftpSharp/Exceptions/TftpErrorResponseException.cs
+++ TftpSharp/Exceptions/TftpErrorResponseException.cs
@@ -2,8 +2,13 @@
 {
     public class TftpErrorResponseException : TftpException
     {
+        public ErrorCode ErrorCode { get; }
+        public string ErrorMessage { get; }
+
         public TftpErrorResponseException(ErrorCode errorCode, string errorMessage) : base($"{(int)errorCode}-{errorMessage}")
         {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
         }
     }
 }
